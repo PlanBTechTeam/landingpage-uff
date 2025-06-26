5c4b0712958e79d49649d18dc2c2073fb9c44425
joaogabrielvianna <joaogabrielvianna05@gmail.com>
2025-06-26 13:56:42 -0300

    feat: Criei um carrosel de eventos

--- src/components/events.tsx
+++ src/components/events.tsx
@@ -9,31 +9,32 @@ export const Events = () => {
       </header>
 
       <div className="flex flex-col gap-12 mt-10">
-        {content.events.slice().reverse().map((e) => (
-          <div key={e.year}>
-            <div className="flex flex-wrap gap-6 justify-center sm:justify-start items-start">
-              {/* Card do Ano */}
-              <div className="w-[200px] h-[265px] bg-[#444444] text-white flex p-4  text-3xl font-bold rounded shadow-md">
-                {e.year}
-              </div>
+  {content.events.slice().reverse().map((e) => (
+    <div key={e.year}>
+      <div className="flex sm:flex-wrap gap-6 sm:justify-start items-start overflow-x-auto sm:overflow-visible scrollbar-hide">
+        {/* Card do Ano */}
+        <div className="flex-shrink-0 w-[200px] h-[265px] bg-[#444444] text-white flex items-center justify-center text-3xl font-bold rounded shadow-md">
+          {e.year}
+        </div>
 
-              {/* Cards de eventos */}
-              {e.events.map((ev, index) => (
-                <div
-                  key={index}
-                  className="w-[200px] h-[265px] bg-white/90 shadow-lg rounded overflow-hidden"
-                >
-                  <img
-                    src={PhotoEvents[ev.photo as keyof typeof PhotoEvents]}
-                    alt={`Evento ${e.year}`}
-                    className="w-full h-full object-cover"
-                  />
-                </div>
-              ))}
-            </div>
+        {/* Cards de eventos */}
+        {e.events.map((ev, index) => (
+          <div
+            key={index}
+            className="flex-shrink-0 w-[200px] h-[265px] bg-white/90 shadow-lg rounded overflow-hidden"
+          >
+            <img
+              src={PhotoEvents[ev.photo as keyof typeof PhotoEvents]}
+              alt={`Evento ${e.year}`}
+              className="w-full h-full object-cover"
+            />
           </div>
         ))}
       </div>
+    </div>
+  ))}
+</div>
+
     </section>
   );
 };
