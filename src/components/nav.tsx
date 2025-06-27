d78b33abca6d321a294dbd8740e1173ff07a1b36
joaogabrielvianna <joaogabrielvianna05@gmail.com>
2025-06-27 17:37:16 -0300

    refactore: Diminui os cards da nav

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -5,17 +5,16 @@ export const Nav = () => {
   return (
     <section className="px-4 sm:px-6 md:px-10 lg:px-20 py-10">
       <div className="max-w-screen-xl mx-auto">
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 justify-center">
           {content.nav.map((item) => (
             <a
               key={item.title}
               href={item.href}
-              className="relative group cursor-pointer rounded overflow-hidden"
-            >
+              className="relative group cursor-pointer rounded overflow-hidden mx-auto">
               <img
                 src={NavPhotos[item.img as keyof typeof NavPhotos]}
                 alt={item.title}
-                className="w-full  object-cover"
+                className="object-cover"
               />
               <div className="absolute top-0 left-0 w-1/2 h-1/2 bg-[#444444] bg-opacity-90 text-white flex items-end justify-start p-3">
                 <h1 className="text-xl sm:text-3xl font-bold leading-tight tracking-wide">
@@ -27,5 +26,6 @@ export const Nav = () => {
         </div>
       </div>
     </section>
+
   );
 };
