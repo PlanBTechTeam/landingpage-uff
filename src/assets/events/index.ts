8f387f6d77ee14cef1635955b2b41c74fb70d133
joaogabrielvianna <joaogabrielvianna05@gmail.com>
2025-07-27 22:27:34 -0300

    feat: Coloquei as imagenns dos eventos em uma arquitetura que ao meu ver facilitará no futuro.

--- src/assets/events/index.ts
+++ src/assets/events/index.ts
@@ -1,40 +1,69 @@
-import StockHammer from './stockhammer.png'
-import Porto from './porto.png'
-import Conferencia from './conferencia.png'
-import Remotra from './remotra.png'
-import ParaAlem from './para-alem.png'
-import Alceste from './alceste.png'
-import AMorte from './a-morte.png'
-import Parir from './parir.png'
-import Andando from './andando.png'
-import Desigualdade from './desigualdade.png'
-import Hibrid from './hibrid.png'
-import Alumbramientos from './alumbramientos.png'
-import Circuitos from './circuitos.png'
-import Joias from './joias.png'
-import Doencas from './doencas.png'
-import Coins from './coins.png'
-import OficinaTreinamentoEmEpidoc from './oficina-treinamento-em-epidoc.png'
-import Estudos from './estudos.png'
-import MiniCursos from './mini-curso.png'
-import Simposio from './simposio.png'
-import Colombani from './colombani.png'
-import Hirota from './hirota.png'
-import Vasques from './vasques.png'
-import Adriana from './adriana.png'
-import OficinaTech from './oficina-tech.png'
-import CursoMedieval from './curso-medieval.png'
-import EcontroGtha from './econtro-gtha.png'
-import Realidade from './realidade.png'
-import Coloquio from './coloquio.png'
-import Moedas from './moedas.png'
-import MaterialidadesExMachina from './materialidades-ex-machina.png'
-import Uff from './uff.png'
-import Parsas from './parsas.png'
-import Virtual from './virtual.png'
+// 2024
+import Porto from './2024/porto.png'
+import Conferencia from './2024/conferencia.png'
+import Remotra from './2024/remotra.png'
+import ParaAlem from './2024/para-alem.png'
+import Alceste from './2024/alceste.png'
+import AMorte from './2024/a-morte.png'
+import Parir from './2024/parir.png'
+import Andando from './2024/andando.png'
+import Desigualdade from './2024/desigualdade.png'
+import Hibrid from './2024/hibrid.png'
+import Alumbramientos from './2024/alumbramientos.png'
+
+// 2023
+import Circuitos from './2023/circuitos.png'
+
+// 2020
+import Doencas from './2020/doencas.png'
+import Estudos from './2020/estudos.png'
+import Joias from './2020/joias.png'
+import MiniCursos from './2020/mini-curso.png'
+
+// 2019
+import Uff from './2019/uff.png'
+import MaterialidadesExMachina from './2019/materialidades-ex-machina.png'
+import Parsas from './2019/parsas.png'
+import Virtual from './2019/virtual.png'
+import OficinaTreinamentoEmEpidoc from './2019/oficina-treinamento-em-epidoc.png'
+
+// 2018
+import Coloquio from './2018/coloquio.png'
+import OficinaRetireSuasAplicações from './2018/OficinaRetireSuasAplicações.jpeg'
+import Moedas from './2018/moedas.png'
+import Coins from './2018/coins.png'
+
+// 2017
+import Realidade from './2017/realidade.png'
+
+// 2016
+import EcontroGtha from './2016/econtro-gtha.png'
+
+// 2015
+import CursoMedieval from './2015/curso-medieval.png'
+import OficinaTech from './2015/oficina-tech.png'
+
+// 2014
+import Adriana from './2014/adriana.png'
+
+// 2013
+import Colombani from './2013/colombani.png'
+import Hirota from './2013/hirota.png'
+import Vasques from './2013/vasques.png'
+import CitricaDaHermeneuticaECulturaMaterial from './2013/CitricaDaHermeneuticaECulturaMaterial.jpeg'
+import OsMosaicosProvinciaisRomanosDaGalia from './2013/OsMosaicosProvinciaisRomanosDaGalia.jpeg'
+
+// 2012
+import VSimpósioNacionalEIVInternacionalDeEstudosCeltasEGermanicos from './2012/VSimpósioNacionalEIVInternacionalDeEstudosCeltasEGermanicos.png'
+
+// 2011
+import ThePowerOfEntanglement from './2011/ThePowerOfEntanglement.png'
+
+// 2009
+import ALinguagemAudioVisualEOEstudoDaHistoriaAntiga from './2009/ALinguagemAudioVisualEOEstudoDaHistoriaAntiga.jpeg'
+
 export const PhotoEvents = {
-    StockHammer,
-    Porto,
+    // 2024
     Conferencia,
     Remotra,
     ParaAlem,
@@ -45,26 +74,59 @@ export const PhotoEvents = {
     Desigualdade,
     Hibrid,
     Alumbramientos,
-    Circuitos,
-    Joias,
-    Doencas,
     Coins,
     OficinaTreinamentoEmEpidoc,
-    Estudos,
-    MiniCursos,
-    Simposio,
-    Colombani,
-    Hirota,
-    Vasques,
-    Adriana,
-    OficinaTech,
-    CursoMedieval,
-    EcontroGtha,
-    Realidade,
     Coloquio,
     Moedas,
-    MaterialidadesExMachina,
+    
+    // 2024
+    Porto,
+    
+    // 2023
+    Circuitos,
+
+    
+    // 2020
+    Doencas,
+    Estudos,
+    Joias,
+    MiniCursos,
+    
+    
+    // 2019
     Uff,
+    MaterialidadesExMachina,
     Parsas,
-    Virtual
+    Virtual,
+    
+    // 2018
+    OficinaRetireSuasAplicações,
+    
+    // 2017
+    Realidade,
+
+    // 2016
+    EcontroGtha,
+
+    // 2015
+    CursoMedieval,
+    OficinaTech,
+
+    // 2014
+    Adriana,
+    
+    // 2013
+    Vasques,
+    Hirota,
+    Colombani,
+    CitricaDaHermeneuticaECulturaMaterial,
+    OsMosaicosProvinciaisRomanosDaGalia,
+    
+    // 2012
+    VSimpósioNacionalEIVInternacionalDeEstudosCeltasEGermanicos,
+    // 2011
+    ThePowerOfEntanglement,
+    
+    // 2009
+    ALinguagemAudioVisualEOEstudoDaHistoriaAntiga
 }
